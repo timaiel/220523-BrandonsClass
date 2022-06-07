f801caf5e34c523fbb3f13c1c5623bb2dca4f5c5
unknown <timaielsmith74@gmail.com>
2022-06-06 20:28:00 -0700

    trying to fix an error

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,7 +1,16 @@
 package com.revature;
+import java.util.Scanner;
 
 public class Driver {
      public static void main(String[] args){
+
          System.out.println("Your Application has started");
+
+         //object initialization
+         Scanner scanner = new Scanner(System.in);
+         String input = scanner.nextLine();
+         System.out.println(input);
+
+
      }
 }
